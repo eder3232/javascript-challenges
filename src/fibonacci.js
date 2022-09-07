a4a4b9632a0f68c5c7f9a21fba0873dfe9c96018
eder3232 <eldebar.dk@gmaill.com>
2022-09-07 06:13:48 -0500

    eder_3232

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,14 @@
 const fibonacci = (n) => {
   // your code here
+  function fibo(number) {
+    if (number === 0) return 0
+    if (number === 1) return 1
+    return fibo(number - 1) + fibo(number - 2)
+  }
+  const arr = []
+  for (let i = 1; i <= n; i++) {
+    arr.push(fibo(i))
+  }
+  return arr
 }
-
-module.exports = fibonacci;
+module.exports = fibonacci
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,12 @@
 const trialDivision = (number) => {
   // your code here
+  if (number < 2) return false
+  if (!Number.isInteger(number)) return false
+  for (let i = 2; i <= number ** 0.5; i++) {
+    if (number % i === 0) return false
+  }
+
+  return true
 }
 
-module.exports = trialDivision;
+module.exports = trialDivision
